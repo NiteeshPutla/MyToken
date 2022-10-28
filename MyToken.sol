de7e03f7a6710927f1ca9998c9a6e430e2d2fdc6
Niteesh Putla <90021910+NiteeshPutla@users.noreply.github.com>
2022-10-28 11:48:33 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier:GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
-
+pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20{
